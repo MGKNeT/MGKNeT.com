a9288d9b91904a44761eee2168a715ef5cf72d90
Gabriel Kanev <contact@mrgkanev.eu>
2017-01-29 13:25:21 +0200

    First "killer" update
    It has alot of things to fix. Upload it so i can see the changes faster.
    :)

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="Just a diffirent way of thinking and making products for the web.">
+      <meta name="keywords" content="web design, web devolepment, mobile applications, hosting projects">
+      <meta name="author" content="MGKNeT">
+	  <meta name="theme-color" content="#ff5722">
+      <title>MGKNeT</title>
+	  <meta property="og:title" content="MGKNeT" />
+	  <meta property="og:description" content="Just a diffirent way of thinking and making products for the web." />
+<!--  <meta property="og:image" content="images/icon.png" /> -->
+      <link rel="canonical" href="">
+      <link rel="icon" type="image/png" href="images/icon.png">
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700|Raleway:300,300i,400,400i,700,700i" rel="stylesheet">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+      <link href="css/stroke7-icon-font.min.css" rel="stylesheet">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css" rel="stylesheet">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.6/assets/owl.carousel.min.css" rel="stylesheet">
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.6/assets/owl.theme.default.min.css" rel="stylesheet">
+	      <script>
+	      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	       })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+	       ga('create', 'UA-70474115-1', 'auto');
+	       ga('send', 'pageview');
+	
+	      </script>
+   </head>
---
+++ index.php
@@ -0,0 +1,158 @@
+<? include "header.php"; ?>
+
+<body class="lt-theme-octobercms-octaskin lt-layout-octaskin lt-page-home">
+    <div id="lt-page-surround">
+        <section id="lt-navigation" class="lt-section section lt-no-background">
+            <div class="lt-row row">
+                <div class="lt-content lt-navigation-content">
+                    <div class="lt-navbar navbar-fixed">
+                        <nav class="lt-no-background">
+                            <div class="nav-wrapper">
+                                <div class="lt-container container">
+                                    <a href="index.php" class="brand-logo white-text"><img src="images/logo-white.png">
+                                    </a>
+                                    <a href="#" data-activates="mobile-menu" class="lt-mobile-toggle button-collapse"><i class="material-icons">menu</i></a>
+                                    <ul class="right hide-on-med-and-down">
+                                        <li class="active"><a href="index.php">Home</a>
+                                        </li>
+                                        <li class=""><a href="/about">About</a>
+                                        </li>
+                                        <li class=""><a href="/portfolio">Portfolio</a>
+                                        </li>
+                                        <li class=""><a href="/contact">Contact</a>
+                                        </li>
+                                    </ul>
+                                    <ul class="side-nav" id="mobile-menu" style="transform: translateX(-100%);">
+                                        <li class="active"><a href="index.php">Home</a>
+                                        </li>
+                                        <li class=""><a href="/about">About</a>
+                                        </li>
+                                        <li class=""><a href="/portfolio">Portfolio</a>
+                                        </li>
+                                        <li class=""><a href="/contact">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="lt-slideshow" class="lt-section lt-section-fullwidth section">
+            <div class="lt-container container">
+                <div class="lt-content lt-slideshow-content">
+                    <div class="lt-row row">
+                        <div class="lt-col col s12">
+                            <div class="lt-slider slider fullscreen" style="touch-action: pan-y; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0); height: 926px; position: relative;">
+                                <ul class="slides">
+                                    <li class="" style="opacity: 0; transform: translateX(0px) translateY(0px);">
+                                        <img src="data:image/gif;base64,R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="image" style="background-image: url(&quot;images/slideshow/img-01.jpg&quot;);">
+                                        <div class="caption center-align" style="opacity: 0; transform: translateY(-100px) translateX(0px);">
+                                            <h2 class="lt-slider-title">Text 1</h2>
+                                            <h5 class="lt-slider-subtitle">Help :)</h5>
+                                        </div>
+                                    </li>
+                                    <li class="active" style="opacity: 1;">
+                                        <img src="data:image/gif;base64,R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="image" style="background-image: url(&quot;images/slideshow/img-02.jpg&quot;);">
+                                        <div class="caption center-align" style="opacity: 1; transform: translateY(0px) translateX(0px);">
+                                            <h2 class="lt-slider-title">Text 1</h2>
+                                            <h5 class="lt-slider-subtitle">Help :)</h5>
+                                        </div>
+                                    </li>
+                                    <li class="" style="opacity: 0; transform: translateX(0px) translateY(0px);">
+                                        <img src="data:image/gif;base64,R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" alt="image" style="background-image: url(&quot;images/slideshow/img-03.jpg&quot;);">
+                                        <div class="caption center-align" style="opacity: 0; transform: translateY(-100px);">
+                                            <h2 class="lt-slider-title">Text 1</h2>
+                                            <h5 class="lt-slider-subtitle">Help :)</h5>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="lt-feature" class="lt-section section">
+            <div class="lt-container container">
+                <div class="lt-content lt-feature-content">
+                    <div class="center">
+                        <div class="lt-row row">
+                            <div class="lt-col col s12">
+                                <h2 class="lt-title">Octaskin Features</h2>
+                                <p class="flow-text">Octaskin comes with the sample pages to help you get started faster. You can change the text and images and you're good to go.</p>
+                            </div>
+                        </div>
+                        <div class="lt-vertical-spacer"></div>
+                        <div class="lt-row row">
+                            <div class="lt-col col m4 s12">
+                                <i class="large pe-7s-edit primary-color-text"></i>
+                                <h5>Customizable</h5>
+                                <p>Develop a system of design that allows for a unified user experience across all their products on any platform.</p>
+                            </div>
+                            <div class="lt-col col m4 s12">
+                                <i class="large pe-7s-science primary-color-text"></i>
+                                <h5>Modern</h5>
+                                <p>Develop a system of design that allows for a unified user experience across all their products on any platform.</p>
+                            </div>
+                            <div class="lt-col col m4 s12">
+                                <i class="large pe-7s-exapnd2 primary-color-text"></i>
+                                <h5>Responsive</h5>
+                                <p>Develop a system of design that allows for a unified user experience across all their products on any platform.</p>
+                            </div>
+                        </div>
+                        <div class="lt-row row">
+                            <div class="lt-col col m4 s12">
+                                <i class="large pe-7s-ribbon primary-color-text"></i>
+                                <h5>Stylish</h5>
+                                <p>Develop a system of design that allows for a unified user experience across all their products on any platform.</p>
+                            </div>
+                            <div class="lt-col col m4 s12">
+                                <i class="large pe-7s-like2 primary-color-text"></i>
+                                <h5>Great Support</h5>
+                                <p>Our #1 priority is you, the client. We believe in our product and hold ourselves to the highest standards. We truly care about your application as much as you do. You can count on us.</p>
+                            </div>
+                            <div class="lt-col col m4 s12">
+                                <i class="large pe-7s-note2 primary-color-text"></i>
+                                <h5>Documented</h5>
+                                <p>Develop a system of design that allows for a unified user experience across all their products on any platform.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="lt-extension" class="lt-section section">
+            <div class="lt-container container">
+                <div class="lt-content lt-extension-content">
+                    <div class="lt-row row">
+                        <div class="lt-col col s12">
+                            <h2 class="lt-title center">Users Voice</h2>
+                        </div>
+                    </div>
+                    <div class="lt-row row">
+                        <div class="col s12">
+                            <div class="lt-owl-carousel owl-carousel owl-theme">
+                                <div class="item">
+                                    <div class="lt-owl-carousel-item item">
+                                        <div class="lt-testimonial-container">
+                                            <div class="lt-testimonial-content flow-text">
+                                                <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit fugiat quia doloribus obcaecati libero eum doloremque neque amet fugit magni</span>
+                                            </div>
+                                            <div class="lt-testimonial-author">
+                                                <div class="lt-testimonial-author-img"><img class="circle responsive-img" src="images/voice/img-02.jpg" alt="user">
+                                                </div>
+                                                <div class="lt-testimonial-author-name">Robert Brown</div>
+                                                <div class="lt-testimonial-author-position">Manager of Desynz</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <? include "footer.php"; ?>
