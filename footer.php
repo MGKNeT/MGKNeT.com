ad98784f73e6b358fc668e3ccb46c3de24134aa5
Gabriel Kanev <contact@mrgkanev.eu>
2017-01-31 16:19:14 +0200

    Footer content
    Yes i forgot the fouter content (dont judge me ).

---
+++ footer.php
@@ -0,0 +1,82 @@
+<section id="lt-footer" class="lt-section section">
+    <div class="lt-container container">
+        <div class="lt-content lt-footer-content">
+            <div class="lt-row row">
+                <div class="lt-col col m5 s12">
+                    <div class="flow-text">Subscribe to get themes updates and exclusive offers.</div>
+                </div>
+                <div class="lt-col col m7 s12">
+                    <!-- Begin MailChimp Signup Form -->
+                    <div id="mc_embed_signup">
+                        <form action="//mgknet.us14.list-manage.com/subscribe/post?u=2d115004f77cb9fac61384bc2&amp;id=f153567d01" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+                            <div id="mc_embed_signup_scroll">
+                                <div class="input-field col m7 s12 mc-field-group">
+                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    <label for="mce-EMAIL">Your Email Address</label>
+                                    <div id="mce-responses" class="clear">
+                                        <div class="response" id="mce-error-response" style="display:none"></div>
+                                        <div class="response" id="mce-success-response" style="display:none"></div>
+                                    </div>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                        <input type="text" name="b_9e48b47c28a1b93742ba12e5a_e0a5daa468" tabindex="-1" value="">
+                                    </div>
+                                </div>
+                                <div class="input-field col m5 s12">
+                                    <button class="waves-effect waves-light btn-large z-depth-0" type="submit" name="subscribe" id="mc-embedded-subscribe">Subscribe <i class="material-icons right">email</i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <!--End mc_embed_signup-->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="lt-copyright" class="lt-section section">
+    <div class="lt-container container">
+        <div class="lt-content lt-copyright-content">
+            <div class="lt-row row">
+                <div class="lt-row row">
+                    <div class="lt-col col s12">
+                        <div>
+                            Made with <a href="#" target="_blank"><i class="fa fa-heart" alt="love"></i></a> by <a href="#" target="_blank">MGKNeT</a>.
+                        </div>
+                        <div>
+                            Powered by <a href="http://materializecss.com/" target="_blank">Materialize CSS</a>.
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="lt-row row">
+                <div class="lt-col col s12">
+                    <div class="lt-social-buttons fixed-action-btn horizontal">
+                        <a class="btn-floating btn-large">
+                            <i class="large material-icons">thumb_up</i>
+                        </a>
+                        <ul>
+                            <li>
+                                <a class="btn-floating blue darken-4" href="https://www.facebook.com/mgknetwork" target="_blank"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li>
+                                <a class="btn-floating blue" href="https://twitter.com/mgknetwork" target="_blank"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li>
+                                <a class="btn-floating black" href="https://github.com/mgknetwork" target="_blank"><i class="fa fa-github"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/graingert-wow/1.2.0/wow.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.6/owl.carousel.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+
+</html>
